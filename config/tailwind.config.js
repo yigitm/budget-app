61daeee276ffe0354e6ff97738ee924b92db9ce3
Yigit Mersin <yigitmersin@gmail.com>
2022-04-20 02:39:54 +0300

    Remove tailwind script

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,19 +11,6 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        black: colors.black,
-        white: colors.white,
-        gray: colors.gray,
-        emerald: colors.emerald,
-        indigo: colors.indigo,
-        yellow: colors.yellow,
-        rose: {
-          600: '#e11d48',
-        },
-      },
     },
   },
   plugins: [
