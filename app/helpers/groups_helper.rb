68920de2c5484042d33c1125e2c2f43a9a42c6a9
Yigit Mersin <yigitmersin@gmail.com>
2022-04-20 02:35:43 +0300

    Add category select menu for icons

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,4 +2,9 @@ module GroupsHelper
   def list_payments(group)
     group.payments.order(created_at: :desc)
   end
+
+  def icons
+     { 'Shopping' => 'shopping.png', 'Travel' => 'travel.png', 'Clothing' => 'clothing.png', 
+      'Grocery' => 'grocery.png' }
+  end
 end
