975b1f0573f1309d991d368169b6a13da366d010
Yigit Mersin <yigitmersin@gmail.com>
2022-04-21 04:48:43 +0300

    Change payment button

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :group_payments, dependent: :destroy
-  has_many :payments, through: :group_payments, dependent: :destroy
+  has_many :payments, through: :group_payments, dependent: :destroy, foreign_key: :group_id
 
   validates :name, presence: true, length: { minimum: 3 }
 
